66bfb566c91aef1dae9bd37fedcc36e673536ff2
DarkElixer <mail.abhisheksikarwar@gmail.com>
2025-06-18 21:49:50 +0530

    fix some bugs

--- app/_components/ProductsGrid.js
+++ app/_components/ProductsGrid.js
@@ -6,11 +6,11 @@ import Error from "./Error";
 
 function ProductsGrid() {
   let { filteredData } = useSelector((state) => state.shopping);
-
+  console.log(filteredData);
   return (
     <div className="flex-auto p-9 ml-5">
       <h1 className="text-[#053762] font-bold text-4xl">Product Listing</h1>
-      {filteredData.length > 0 ? (
+      {filteredData?.length > 0 ? (
         <div className="grid grid-cols-[repeat(auto-fill,minmax(280px,1fr))] gap-6 md:gap-6 my-5">
           {filteredData.map((item) => (
             <Card key={item.id} product={item} />
